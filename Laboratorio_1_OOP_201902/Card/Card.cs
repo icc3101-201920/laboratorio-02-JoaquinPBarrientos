2737fe74558d3f23f058a3bb492921493c6bedf1
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-08-16 20:48:11 -0400

    LAB02

---
+++ Laboratorio_1_OOP_201902/Card/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Laboratorio_1_OOP_201902.Card
+{
+    public class Card
+    {
+        protected string name;
+        protected string type;
+
+    public Card(string name,string type)
+        {
+            Name = name;
+            Type = type;
+        }
+        
+    public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+
+
+
+    }
+}
+
---
+++ Laboratorio_1_OOP_201902/Deck/Hand/Hand.cs
@@ -0,0 +1,21 @@
+﻿using Laboratorio_1_OOP_201902.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902
+{
+    public class Hand : Deck
+    {
+        private const int MAX_HAND_CARDS = 5;
+       
+
+        public Hand(List<CombatCard> cc,  List<SpecialCard> sc) :base(cc,sc)
+        {
+        }
+
+
+        
+    }
+}
+
